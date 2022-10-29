2fb4f7282db37823f7e96885018065260de02592
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-30 00:42:45 +0200

    add drop menu button, so that menu acctualy drops on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [showDropMenu, setShowDropMenu] = useState(false);
@@ -8,6 +8,18 @@ const Navbar = () => {
   const handleDropMenu = () => {
     setShowDropMenu(!showDropMenu);
   };
+
+  useEffect(() => {
+    const navbarEl = document.querySelector(".navbar");
+    const dropMenuEl = document.querySelector(".navbar-drop-menu");
+    const vh = window.outerHeight;
+    let dropMenuHeight = vh - navbarEl.clientHeight;
+    if (showDropMenu) {
+      dropMenuEl.style.height = `${dropMenuHeight}px`;
+    } else {
+      dropMenuEl.style.height = `0px`;
+    }
+  }, [showDropMenu]);
 
   return (
     <div className='navbar-container'>
